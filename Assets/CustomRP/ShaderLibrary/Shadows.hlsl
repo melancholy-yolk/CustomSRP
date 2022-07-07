5b462aaa8de45f54b8ee4944f131fa63c1bb10fd
melancholy-yolk <1027278616@qq.com>
2022-07-07 19:10:06 +0800

    修改聚光灯实时阴影

--- Assets/CustomRP/ShaderLibrary/Shadows.hlsl
+++ Assets/CustomRP/ShaderLibrary/Shadows.hlsl
@@ -14,10 +14,23 @@
 	#define DIRECTIONAL_FILTER_SETUP SampleShadow_ComputeSamples_Tent_7x7
 #endif
 
+#if defined(_OTHER_PCF3)
+    #define OTHER_FILTER_SAMPLES 4
+    #define OTHER_FILTER_SETUP SampleShadow_ComputeSamples_Tent_3x3
+#elif defined(_OTHER_PCF5)
+    #define OTHER_FILTER_SAMPLES 9
+    #define OTHER_FILTER_SETUP SampleShadow_ComputeSamples_Tent_5x5
+#elif defined(_OTHER_PCF7)
+    #define OTHER_FILTER_SAMPLES 16
+    #define OTHER_FILTER_SETUP SampleShadow_ComputeSamples_Tent_7x7
+#endif
+
 #define MAX_SHADOWED_DIRECTIONAL_LIGHT_COUNT 4
+#define MAX_SHADOWED_OTHER_LIGHT_COUNT 16
 #define MAX_CASCADE_COUNT 4
 
 TEXTURE2D_SHADOW(_DirectionalShadowAtlas);
+TEXTURE2D_SHADOW(_OtherShadowAtlas);
 #define SHADOW_SAMPLER sampler_linear_clamp_compare
 SAMPLER_CMP(SHADOW_SAMPLER);
 
@@ -26,6 +39,7 @@ CBUFFER_START(_CustomShadows)
 	float4 _CascadeCullingSpheres[MAX_CASCADE_COUNT];
 	float4 _CascadeData[MAX_CASCADE_COUNT];
 	float4x4 _DirectionalShadowMatrices[MAX_SHADOWED_DIRECTIONAL_LIGHT_COUNT * MAX_CASCADE_COUNT];
+	float4x4 _OtherShadowMatrices[MAX_SHADOWED_OTHER_LIGHT_COUNT];
 	float4 _ShadowAtlasSize;
 	float4 _ShadowDistanceFade;
 CBUFFER_END
@@ -43,6 +57,7 @@ struct DirectionalShadowData
 struct OtherShadowData
 {
 	float strength;
+	int tileIndex;
 	int shadowMaskChannel;
 };
 
@@ -67,6 +82,11 @@ float SampleDirectionalShadowAtlas(float3 positionSTS)
 	return SAMPLE_TEXTURE2D_SHADOW(_DirectionalShadowAtlas, SHADOW_SAMPLER, positionSTS);
 }
 
+float SampleOtherShadowAtlas(float3 positionSTS) 
+{
+	return SAMPLE_TEXTURE2D_SHADOW(_OtherShadowAtlas, SHADOW_SAMPLER, positionSTS);
+}
+
 float FilterDirectionalShadow(float3 positionSTS)
 {
 	#if defined(DIRECTIONAL_FILTER_SETUP)
@@ -82,6 +102,24 @@ float FilterDirectionalShadow(float3 positionSTS)
 		return shadow;
 	#else
 		return SampleDirectionalShadowAtlas(positionSTS);
+	#endif
+}
+
+float FilterOtherShadow(float3 positionSTS)
+{
+	#if defined(OTHER_FILTER_SETUP)
+		real weights[OTHER_FILTER_SAMPLES];
+		real2 positions[OTHER_FILTER_SAMPLES];
+		float4 size = _ShadowAtlasSize.wwzz;
+		OTHER_FILTER_SETUP(size, positionSTS.xy, weights, positions);
+		float shadow = 0;
+		for(int i = 0; i < OTHER_FILTER_SAMPLES; i++)
+		{
+			shadow += weights[i] * SampleOtherShadowAtlas(float3(positions[i].xy, positionSTS.z));//bilinear filter
+		}
+		return shadow;
+	#else
+		return SampleOtherShadowAtlas(positionSTS);
 	#endif
 }
 
@@ -192,9 +230,12 @@ float GetDirectionalShadowAttenuation(DirectionalShadowData directional, ShadowD
     return shadow;
 }
 
-float GetOtherShadow(OtherShadowData other_shadow_data, ShadowData global, Surface surfaceWS)
+float GetOtherShadow(OtherShadowData other, ShadowData global, Surface surfaceWS)
 {
-	return 1.0;
+	float3 normalBias = surfaceWS.interpolatedNormal * 0.0;
+	float4 positionSTS = mul(_OtherShadowMatrices[other.tileIndex], float4(surfaceWS.position + normalBias, 1.0));
+	//return FilterOtherShadow(positionSTS.xyz / positionSTS.w);
+	return SampleOtherShadowAtlas(positionSTS);
 }
 
 float GetOtherShadowAttenuation(OtherShadowData other_shadow_data, ShadowData global, Surface surfaceWS)
@@ -205,7 +246,8 @@ float GetOtherShadowAttenuation(OtherShadowData other_shadow_data, ShadowData gl
 
 	float shadow;
 	// 片段阴影强度<=0 超过了最大阴影距离
-	if (other_shadow_data.strength > 0.0)// other_shadow_data.strength * global.strength <= 0
+	//if (other_shadow_data.strength > 0.0)// other_shadow_data.strength * global.strength <= 0
+	if (other_shadow_data.strength * global.strength <= 0)
 	{
 		shadow = GetBakedShadow(global.shadowMask, other_shadow_data.shadowMaskChannel, other_shadow_data.strength);
 	}
