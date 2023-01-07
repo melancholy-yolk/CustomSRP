c302454f04d65ae5649904ef94871521d8a4f3f8
weibincui <1027278616@qq.com>
2023-01-08 00:28:14 +0800

    随便写写Graph脚本
    随便写写Graph脚本

--- Assets/VisualScript/Runtime/Graph.cs
+++ Assets/VisualScript/Runtime/Graph.cs
@@ -5,7 +5,26 @@ using UnityEngine;
 namespace VisualScript.Runtime
 {
     public class Graph
-    {   
-        
+    {
+        private List<Node> _nodes;
+        public List<Node> nodes => _nodes;
+
+        private List<Connection> _connections;
+        public List<Connection> connections => _connections;
+
+        public virtual void OnGraphStart()
+        {
+
+        }
+
+        public virtual void OnGraphUpdate()
+        {
+
+        }
+
+        public virtual void OnGraphStop()
+        {
+
+        }
     }
 }
